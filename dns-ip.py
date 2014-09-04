9668226e69b24d7f258e6221a441d04418f608b7
Alfredo Giscombe <Mrpnut08@gmail.com>
2014-09-03 22:58:53 -0500

    Finished the program.

--- dns-ip.py
+++ dns-ip.py
@@ -14,7 +14,7 @@ class DNSIPGui(tkinter.Frame):
 		self.createWidgets()
 	
 	#~~ def __init__(self, master=None)
-	
+			
 	def createWidgets(self):
 	
 		self.input_label = tkinter.Label(self, text = "Enter domain name:")
@@ -23,14 +23,14 @@ class DNSIPGui(tkinter.Frame):
 		self.domain_textbox = tkinter.Entry(self)
 		self.domain_textbox.pack()
 		
-		self.resolve_button = tkinter.Button(self, text= "Find IP addresses", command="resolver_click")
+		self.resolve_button = tkinter.Button(self, text= "Find IP addresses", command= self.resolver_click)
 		self.resolve_button.pack()
 		
 		self.addresses_panel = tkinter.Frame(self,self)
 		self.addresses_panel.pack()
 		
 		self.addresses_label = tkinter.Label(self.addresses_panel, text = "Pending domain name")
-		self.addresses_label.grid(columnspan=2)
+		self.addresses_label.grid(row=0, columnspan=2)
 		
 		self.ipv4_label = tkinter.Label(self.addresses_panel, text= "IPv4")
 		self.ipv4_label.grid(row=1, column=0)
@@ -41,12 +41,29 @@ class DNSIPGui(tkinter.Frame):
 		self.ipv4_list = tkinter.Listbox(self.addresses_panel)
 		self.ipv4_list.grid(row=2, column=0)
 		
-		self.ipv6_list = tkinter.Listbox(self.addresses_panel)
+		self.ipv6_list = tkinter.Listbox(self.addresses_panel, width= 35)
 		self.ipv6_list.grid(row=2, column=1)
-	#~~ def createWidgets(self)
-
+	#~~ def createWidgets(self)		
+			
+	def resolver_click(self):
+		domain_name = self.domain_textbox.get()
+		self.addresses_label["text"] = "IP addresses for " + domain_name
+		self.dns_resolver.setDNSName(domain_name)
+		self.dns_resolver.resolve()
+			
+		self.populateListbox(self.ipv4_list, self.dns_resolver.ipv4_addresses)
+		self.populateListbox(self.ipv6_list, self.dns_resolver.ipv6_addresses)
+		
+	def populateListbox(self, listbox, items):
+		if listbox.size() > 0:
+			listbox.delete(0, listbox.size()-1)
+			
+		for item in items:
+			listbox.insert(0, item)
+	
 #~~ class DNSIPGUI (tkinter.Frame)
 
 root = tkinter.Tk()
+root.wm_title("DNS/IP Looker")
 app = DNSIPGui(root)
 app.mainloop()
